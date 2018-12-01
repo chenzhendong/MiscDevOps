7ce918df22f8fb0eefa9250e0d2be351f5d1ff28
chenzhendong <chenzhendong@yahoo.com>
2018-12-01 00:14:51 -0500

    Update cmd.sh

--- jupyterhub/cmd.sh
+++ jupyterhub/cmd.sh
@@ -5,3 +5,11 @@ yum install python36-devel python36-setuptools
 easy_install-3.6 pip
 
 certbot certonly --standalone --preferred-challenges http -d domain
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+nvm ls-remote  --lts
+nvm install v10.14.1
+npm install -g configurable-http-proxy
+
+docker pull jupyterhub/singleuser:0.9
